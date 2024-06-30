2b1a5ef064d2d287f662a67587c632eedb522985
m00nwtchr <m00nwtchr@duck.com>
2024-06-30 22:09:21 +0200

    Yet Another Retrieve Refactor

--- server/src/pipeline/retrieve.rs
+++ server/src/pipeline/retrieve.rs
@@ -5,9 +5,7 @@ use rss::Channel;
 use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
 use std::cmp::min;
-use std::thread::available_parallelism;
 use tracing::Instrument;
-use url::Url;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Retrieve<I> {
@@ -23,40 +21,38 @@ impl<I: Node> Retrieve<I> {
 	}
 }
 
+async fn get_content(mut item: rss::Item, selector: &Selector) -> anyhow::Result<rss::Item> {
+	let Some(link) = &item.link else {
+		return Ok(item);
+	};
+
+	tracing::info!("{link}");
+	let content = reqwest::get(link).await?.text().await?;
+	let html = Html::parse_document(&content);
+	let content: String = html.select(selector).map(|s| s.inner_html()).collect();
+
+	item.description = None;
+	item.content = Some(content);
+
+	Ok(item)
+}
+
 #[async_trait]
 impl<I: Node<Item = Channel>> Node for Retrieve<I> {
 	type Item = Channel;
 
 	async fn run(&self) -> anyhow::Result<Channel> {
 		let mut rss = self.child.run().await?;
-		let n = min(rss.items.len(), available_parallelism()?.get() / 2); // Avoiding too high values to prevent spamming the target site.
+		let n = min(rss.items.len(), 6); // Avoiding too high values to prevent spamming the target site.
 
 		let span = tracing::info_span!("retrieve_node");
-		rss.items = stream::iter(rss.items.into_iter())
-			.map(|mut item| async {
-				let Some(link) = &item.link else {
-					return item;
-				};
-
-				tracing::info!("{link}");
-				let content = reqwest::get(link.parse::<Url>().unwrap())
-					.await
-					.unwrap()
-					.text()
-					.await
-					.unwrap();
-				let html = Html::parse_document(&content);
-				let content: String = html.select(&self.content).map(|s| s.inner_html()).collect();
-
-				item.description = None;
-				item.content = Some(content);
-
-				item
-			})
+		let items: Vec<anyhow::Result<rss::Item>> = stream::iter(rss.items.into_iter())
+			.map(|item| get_content(item, &self.content))
 			.buffered(n)
 			.collect()
 			.instrument(span)
 			.await;
+		rss.items = items.into_iter().collect::<anyhow::Result<_>>()?;
 
 		Ok(rss)
 	}
